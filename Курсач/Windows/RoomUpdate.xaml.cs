64944954b60af7d387384efdc36074437f0dbe1a
MelovZaebl <sanchik74rus@gmail.com>
2021-11-25 20:25:34 +0500

    dnd

--- Курсач/Windows/RoomUpdate.xaml.cs
+++ Курсач/Windows/RoomUpdate.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace Курсач.Windows
 {
@@ -26,6 +26,36 @@ namespace Курсач.Windows
             Room = room;
             DataContext = room;
             CBClasses.ItemsSource = MainWindow.DB.Classes.ToList();
+        }
+
+        public readonly string DirPath = AppDomain.CurrentDomain.BaseDirectory;
+
+        private void ImgDrop(object sender, DragEventArgs e)
+        {
+            Img.Items.Clear();
+            string file_pathes = (string)e.Data.GetData(DataFormats.FileDrop); //массив file_pathes содержит пути к выбранным файлам или папкам при попытке перетащить
+            string new_path = DirPath + "Pics/" + Path.GetFileName(file_pathes); //новый путь файла
+            File.Copy(file_pathes, new_path); //копирование файла
+            Img.Items.Add(new FileObject(Path.GetFileName(file_pathes), new_path)); //добавление в список элемента FileObject (который содержит информацию о файле)
+            Room.Photo = new_path;
+        }
+
+        class FileObject
+        {
+            public string Name { get; set; }
+
+            public string FullName { get; set; }
+
+            public FileObject(string name, string fullname)
+            {
+                Name = name;
+                FullName = fullname;
+            }
+
+            public override string ToString()
+            {
+                return Name;
+            }
         }
     }
 }
