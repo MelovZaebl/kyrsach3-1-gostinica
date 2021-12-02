1f983f69465d37b91b74411d55eb86a99462a2f2
MelovZaebl <sanchik74rus@gmail.com>
2021-12-02 11:46:20 +0500

    Ордера начало логики

--- Курсач/Windows/OrderAdd.xaml.cs
+++ Курсач/Windows/OrderAdd.xaml.cs
@@ -19,10 +19,47 @@ namespace Курсач.Windows
     /// </summary>
     public partial class OrderAdd : Window
     {
+        public OrdersReg Order = new OrdersReg();
+        public Lodgers Lodger = new Lodgers();
         public OrderAdd()
         {
             InitializeComponent();
             CStartD.SelectedDate = DateTime.Now;
+            DataContext = Order;
+            CBRoom.ItemsSource = MainWindow.DB.Rooms.ToList();
+        }
+
+        private void RoomChange(object sender, SelectionChangedEventArgs e)
+        {
+            CBGuest.Items.Clear();
+            Rooms SelectedRoom = CBRoom.SelectedItem as Rooms;
+            var z = MainWindow.DB.Classes.ToList().Where(p => p.ClassID == SelectedRoom.Class).First();
+            for (int i = 0; i < z.SpotsCount - 1; i++)
+            {
+                CBGuest.Items.Add(i);
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            string error = String.Empty;
+            if (String.IsNullOrWhiteSpace(TBFIO.Text) || TBFIO.Text.Count() > 50)
+            {
+                error += "Введите ФИО.\n";
+            }
+            if (String.IsNullOrWhiteSpace(TBPassport.Text) || TBPassport.Text.Count() > 10)
+            {
+                error += "Введите паспорт\n";
+            }
+            if (String.IsNullOrWhiteSpace(TBPhone.Text) || TBPhone.Text.Count() != 11)
+            {
+                error += "Введите телефон\n";
+            }
+            if (error != "")
+            {
+                MessageBox.Show(error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
         }
     }
 }
